cdd7ee99bd6ebb5e8400b44bcc5f34c400c9dae5
Erik Medeiros <erik.medeiros@gbmlogistica.com.br>
2025-02-23 18:13:19 -0300

    add try/catch

--- backend/index.ts
+++ backend/index.ts
@@ -15,51 +15,66 @@ app.get("/ping", (_, response) => {
 app.post("/employee", async (request, response) => {
     const { username, name, position, salary, isAdmin, password } = request.body;
 
-    const existing = await db.selectFrom("employee")
-        .where('username', '=', username)
-        .select('id')
-        .executeTakeFirst();
+    try {
+        const existing = await db.selectFrom("employee")
+            .where('username', '=', username)
+            .select('id')
+            .executeTakeFirst();
 
-    if (!!existing) {
-        response.send({ message: "login já utilizado" });
-        return;
+        if (!!existing) {
+            response.send({ message: "login já utilizado" });
+            return;
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const employee = await db.insertInto("employee")
+            .values({ username, name, position, salary, isAdmin: isAdmin === 'true' ? 1 : 0, password: hashedPassword })
+            .returning('id')
+            .executeTakeFirstOrThrow()
+
+        response.send({ employeeId: employee.id });
+    } catch(error) {
+        const message = "erro ao criar um funcionário"
+        response.status(500).send({ message });
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const employee = await db.insertInto("employee")
-        .values({ username, name, position, salary, isAdmin: isAdmin === 'true' ? 1 : 0, password: hashedPassword })
-        .returning('id')
-        .executeTakeFirstOrThrow()
-
-    response.send({ employeeId: employee.id });
 });
 
 app.put("/employee/:id", async (request, response) => {
     const id = request.params.id;
     const { name, position, salary, isAdmin } = request.body;
 
-    const employee = await db.updateTable("employee")
-        .where('id', '=', +id)
-        .set({ name, position, salary, isAdmin })
-        .returning('id')
-        .executeTakeFirstOrThrow()
+    try {
+        const employee = await db.updateTable("employee")
+            .where('id', '=', +id)
+            .set({ name, position, salary, isAdmin })
+            .returning('id')
+            .executeTakeFirstOrThrow()
 
-    response.send({ employeeId: employee.id });
+        response.send({ employeeId: employee.id });
+    } catch(error) {
+        const message = "erro ao atualizar um funcionário"
+        response.status(500).send({ message });
+    }
 });
 
 app.get("/employee/:username/:password", async (request, response) => {
     const { username, password } = request.params;
 
-    const user = await db.selectFrom("employee")
-        .where('name', '=', username)
-        .select('password')
-        .executeTakeFirstOrThrow();
+    try {
+        const user = await db.selectFrom("employee")
+            .where('name', '=', username)
+            .select('password')
+            .executeTakeFirstOrThrow();
 
-    const match = await bcrypt.compare(password, user.password);
-    
-    response.send({ login: match });
+        const match = await bcrypt.compare(password, user.password);
+        
+        response.send({ login: match });
+    } catch(error) {
+        const message = "login e/ou senha incorreta"
+        response.status(500).send({ message });
+    }
 });
 
 app.get("/employees", async (_, response) => {
@@ -73,25 +88,35 @@ app.get("/employees", async (_, response) => {
 app.post("/storage", async (request, response) => {
     const { productName, quantity, unitPrice  } = request.body;
 
-    const storage = await db.insertInto("storage")
-        .values({ productName, quantity, unitPrice })
-        .returning("id")
-        .executeTakeFirst();
+    try {
+        const storage = await db.insertInto("storage")
+            .values({ productName, quantity, unitPrice })
+            .returning("id")
+            .executeTakeFirstOrThrow();
 
-    response.send({ storageId: storage?.id });
+        response.send({ storageId: storage.id });
+    } catch(error) {
+        const message = "erro ao criar um item no armazem"
+        response.status(500).send({ message });
+    }
 });
 
 app.put("/storage/:id", async (request, response) => {
     const id = request.params.id;
     const { productName, quantity, unitPrice  } = request.body;
 
-    const storage = await db.updateTable("storage")
-        .where("id", "=", +id)
-        .set({ productName, quantity, unitPrice })
-        .returning("id")
-        .executeTakeFirstOrThrow();
+    try {
+        const storage = await db.updateTable("storage")
+            .where("id", "=", +id)
+            .set({ productName, quantity, unitPrice })
+            .returning("id")
+            .executeTakeFirstOrThrow();
 
-    response.send({ storageId: storage.id });
+        response.send({ storageId: storage.id });
+    } catch(error) {
+        const message = "erro ao atualizar um item do armazem"
+        response.status(500).send({ message });
+    }
 });
 
 app.get("/storages", async (_, response) => {
