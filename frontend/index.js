70459013663715fdc66560478a2d89220b7b8295
Erik Medeiros <erikmedeiros4@gmail.com>
2025-03-26 19:04:35 -0300

    fix base url

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000" //"https://backend-production-cec8.up.railway.app";
+const BASE_URL = "https://backend-production-cec8.up.railway.app";
 
 async function isAdmin() {
     const id = window.localStorage.getItem("token");
